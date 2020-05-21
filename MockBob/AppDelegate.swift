f9b231f1aea7a4070f30713c937df99e9a0e480b
Jonathan Grider <jgrider2015@gmail.com>
2020-05-20 23:47:57 -0400

    Initial Commit

---
+++ MockBob/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MockBob
+//
+//  Created by Jonathan Grider on 5/20/20.
+//  Copyright © 2020 Jonathan Grider. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
