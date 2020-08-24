472e38e9ff9d24a9a1c06fe867ff65d38538b4ee
Jonathan Grider <jgrider2015@gmail.com>
2020-08-23 22:55:00 -0400

    Added mouse hover tracking for OS X 10.14 and later

--- MockBob/Extensions/MBPointerButton.swift
+++ MockBob/Extensions/MBPointerButton.swift
@@ -22,7 +22,31 @@ class MBPointerButton: NSButton {
         if let image = self.image {
             image.isTemplate = true
         }
+        
+        // Add tracking area for hover effects on OS X 10.14 and later
+        // Could also include the cursor updates in this tracking but kept it separate for the sake of recording examples
+        if #available(OSX 10.14, *) {
+            let trackingArea = NSTrackingArea(rect: bounds, options: [.activeAlways, .mouseEnteredAndExited], owner: self, userInfo: nil)
+            self.addTrackingArea(trackingArea)
+        }
+    }
+    
+    override func mouseEntered(with event: NSEvent) {
+        super.mouseEntered(with: event)
+        if #available(OSX 10.14, *) {
+            if effectiveAppearance.name == .darkAqua || effectiveAppearance.name == .vibrantDark {
+                self.contentTintColor = .white
+            } else {
+                self.contentTintColor = .black
+            }
+        }
+    }
 
+    override func mouseExited(with event: NSEvent) {
+        super.mouseExited(with: event)
+        if #available(OSX 10.14, *) {
+            self.contentTintColor = .none
+        }
     }
     
     override func resetCursorRects() {
